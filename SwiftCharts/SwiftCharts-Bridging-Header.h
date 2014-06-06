21b1102baf62bd3daaa7da56c037b7b2296ae610
Sam Davies <sdavies@shinobicontrols.com>
2014-06-05 23:08:00 -0700

    Add the bridging header to the project

---
+++ SwiftCharts/SwiftCharts-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  SwiftCharts-Bridging-Header.h
+//  SwiftCharts
+//
+//  Created by Sam Davies on 05/06/2014.
+//  Copyright (c) 2014 ShinobiControls. All rights reserved.
+//
+
+#import <ShinobiCharts/ShinobiChart.h>
