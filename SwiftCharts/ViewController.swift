40dc890e8ee9476603613f233fcb937170e44ed6
Sam Davies <sdavies@shinobicontrols.com>
2014-06-05 23:42:15 -0700

    Create chart in swift

--- SwiftCharts/ViewController.swift
+++ SwiftCharts/ViewController.swift
@@ -8,18 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SChartDatasource {
                             
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    let chart = ShinobiChart(frame: view.bounds)
+    chart.licenseKey = "<YOUR LICENSE KEY HERE>"
+    chart.datasource = self
+    chart.autoresizingMask = .FlexibleHeight | .FlexibleWidth
+    view.addSubview(chart)
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+    
+  /* SChartDatasource methods */
+  func numberOfSeriesInSChart(chart: ShinobiChart!) -> Int {
+    return 1
   }
-
-
+    
+  func sChart(chart: ShinobiChart!, seriesAtIndex index: Int) -> SChartSeries! {
+    return SChartLineSeries()
+  }
+    
+  func sChart(chart: ShinobiChart!, numberOfDataPointsForSeriesAtIndex seriesIndex: Int) -> Int {
+    return 100
+  }
+    
+  func sChart(chart: ShinobiChart!, dataPointAtIndex dataIndex: Int, forSeriesAtIndex seriesIndex: Int) -> SChartData! {
+    let dp = SChartDataPoint()
+    dp.xValue = dataIndex
+    dp.yValue = dataIndex * dataIndex
+    return dp
+  }
 }
 
